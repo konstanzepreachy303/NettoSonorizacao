64b2874d7e1c152a0e6a28dcf750837c4602f82e
konstanzepreachy303 <biel0211000@gmail.com>
2026-03-21 14:31:54 -0300

    Update testebanco.php

--- testebanco.php
+++ testebanco.php
@@ -5,13 +5,17 @@ $dbname = getenv('MYSQLDATABASE');
 $user = getenv('MYSQLUSER');
 $password = getenv('MYSQLPASSWORD');
 
-echo "HOST: $host <br>";
-echo "PORT: $port <br>";
-echo "DB: $dbname <br>";
-echo "USER: $user <br>";
+echo "HOST: " . $host . "<br>";
+echo "PORT: " . $port . "<br>";
+echo "DB: " . $dbname . "<br>";
+echo "USER: " . $user . "<br><br>";
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
     echo "CONECTOU NO BANCO!";
 } catch (PDOException $e) {
     echo "ERRO: " . $e->getMessage();
